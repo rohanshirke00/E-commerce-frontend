8ebe90fa6580333a650e5e0e426b17361361d626
Rohan Shirke <rohanshirke25812@gmail.com>
2024-05-18 19:12:34 +0530

    Product details page added

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const router = createBrowserRouter([
     element: <Checkout></Checkout>,
   },
   { 
-    path: '/product-detail',
+    path: '/product-detail/:id',
     element: <ProductDetailPage></ProductDetailPage>,
   },
 ]);
